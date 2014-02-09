a5dbded83e091096c1e8dfb546e719d8c492526f
jason.kok <kokatoo1948@gmail.com>
2014-02-09 20:53:40 +0800

    Forgot about deleted files.

--- app/models/expedia.rb
+++
@@ -1,3 +0,0 @@
-class Expedia < ActiveRecord::Base
-  # attr_accessible :title, :body
-end
--- db/migrate/20140205125004_create_expedia.rb
+++
@@ -1,8 +0,0 @@
-class CreateExpedia < ActiveRecord::Migration
-  def change
-    create_table :expedia do |t|
-
-      t.timestamps
-    end
-  end
-end
