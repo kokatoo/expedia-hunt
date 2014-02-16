4164ada6c08cbd9ecdb31e797ad07d7efccee63a
jason.kok <kokatoo1948@gmail.com>
2014-02-16 22:01:09 +0800

    Move logic to model.

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -39,68 +39,11 @@ class SearchesController < ApplicationController
 
 	def start
 		@old_search = Search.find(params[:id])
-		@search = @old_search.dup
-		@search.version = @old_search.version += 1
+		@search = Search.duplicate(@old_search)
 
-		agent = Mechanize.new
-		# note the date format is different
-		flight_url = "http://www.expedia.com/Flight-Search-All?action=FlightSearchAll%40searchFlights&origref=www.expedia.com%2FFlight-Search-All&inpFlightRouteType=2&inpDepartureLocations=#{CGI.escape @search.source}&inpArrivalLocations=#{CGI.escape @search.destination}&inpDepartureDates=#{CGI.escape @search.start.strftime("%m/%d/%Y")}&inpArrivalDates=#{CGI.escape (@search.start + @search.min.days).strftime("%m/%d/%Y")}&inpAdultCounts=1&inpChildCounts=0&inpChildAges=-1&inpChildAges=-1&inpChildAges=-1&inpChildAges=-1&inpChildAges=-1&inpInfants=2&inpFlightAirlinePreference=&inpFlightClass=3"
-		agent.get(flight_url)
+		@search.load_flights()
 
-		url = "http://www.expedia.com/Flight-Search-Outbound?#{agent.page.search('form#flightResultForm')[0]['action'].split('?')[1]}"
-		json = JSON.load(open(url))
-
-	  json["searchResultsModel"]["offers"][0..5].each_with_index do |result, index|
-	  	flight = Flight.new()
-	  	flight.url = url
-	  	flight.start = @search.start.strftime("%d/%m/%Y")
-	  	flight.end = (@search.start + @search.min.days).strftime("%d/%m/%Y")
-	  	flight.source = @search.source
-	  	flight.destination = @search.destination
-
-  		leg = result["legs"][0]
-  		flight.price = leg["price"]["offerPrice"]
-  		flight.currency = leg["price"]["localizedCurrencyCode"]
-
-  		timelines = leg["timeline"]
-  		flight.timelines = []
-  		timelines.each do |timeline|
-
-  			tl = Timeline.new
-
-  			if timeline.has_key?("carrier")
-  				tl.layover = false
-
-  				tl.airline = timeline["carrier"]["airlineName"]	  		
-  				tl.departure = timeline["departureAirport"]["longName"]
-  				tl.arrival = timeline["arrivalAirport"]["longName"]
-
-  				start_time = timeline["departureTime"]
-  				end_time = timeline["arrivalTime"]
-
-  				tl.start = DateTime.parse("#{start_time["dateLongStr"]} #{start_time['time']}")
-  				tl.end = DateTime.parse("#{end_time["dateLongStr"]} #{end_time['time']}")
-  			else
-  				tl.layover = true
-  				tl.departure = timeline["airport"]["longName"]
-  				tl.arrival = timeline["airport"]["longName"]
-
-  				start_time = timeline["startTime"]
-  				end_time = timeline["endTime"]
-
-  				tl.start = DateTime.parse("#{start_time["dateLongStr"]} #{start_time['time']}")
-  				tl.end = DateTime.parse("#{end_time["dateLongStr"]} #{end_time['time']}")
-  			end
-  			tl.save!
-
-  			flight.timelines << tl
-  		end
-  		flight.save!
-  		@search.flights << flight
-  		p @search.flights
-  		p flight
-  		@search.save!
-	  end
+		@search.save!
 
 		redirect_to @search
 	end
