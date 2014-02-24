70b9680b0cd940e589c5b516c12ae45f18e33ad5
jason.kok <kokatoo1948@gmail.com>
2014-02-24 23:13:25 +0800

    Configure to use heroku redis.

--- config/initializers/resque.rb
+++ config/initializers/resque.rb
@@ -1,4 +1,9 @@
 Dir[File.join(Rails.root, 'app', 'jobs', '*.rb')].each { |file| require file }
 
-config = YAML::load(File.open("#{Rails.root}/config/redis.yml"))
-Resque.redis = Redis.new(:host => config['host'], :port => config['port'])
+if ENV["REDISCLOUD_URL"]
+	uri = URI.parse(ENV["REDISCLOUD_URL"])
+	$redis = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
+else
+	config = YAML::load(File.open("#{Rails.root}/config/redis.yml"))
+	Resque.redis = Redis.new(:host => config['host'], :port => config['port'])
+end
