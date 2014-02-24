1fddab2bab89d48f3fbdad18d7885628f5012f57
jason.kok <kokatoo1948@gmail.com>
2014-02-25 00:03:05 +0800

    Try Resque.redis

--- config/initializers/resque.rb
+++ config/initializers/resque.rb
@@ -2,7 +2,7 @@ Dir[File.join(Rails.root, 'app', 'jobs', '*.rb')].each { |file| require file }
 
 if ENV["REDISCLOUD_URL"]
 	uri = URI.parse(ENV["REDISCLOUD_URL"])
-	$redis = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
+	Resque.redis = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
 else
 	config = YAML::load(File.open("#{Rails.root}/config/redis.yml"))
 	Resque.redis = Redis.new(:host => config['host'], :port => config['port'])
